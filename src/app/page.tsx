8d311f807cbf46add1820c47bd4361006d61939d
priyanshu <priyanshu1290singh@gmail.com>
2025-02-08 15:49:09 +0530

    login complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-"use client";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+  const {isAuthenticated} = getKindeServerSession();
+
+if (!(await isAuthenticated())) {
+  return redirect(
+    "/api/auth/login?post_login_redirect_url=http://localhost:3000/page/profile"
+  );
+}
   return (
-    <>
-      Landing Page
-    </>
+    <div>
+      <LoginLink>Sign in</LoginLink>
+
+<RegisterLink>Sign up</RegisterLink>
+    </div>
   );
 }
