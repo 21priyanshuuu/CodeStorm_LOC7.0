92be32f5d55fe0e3f772e8106c4134c9e14425ba
Meetteem <meetpanchal000004@gmail.com>
2025-02-08 14:08:52 +0530

    Structure

---
+++ src/app/page/filecase/page.js
@@ -0,0 +1,11 @@
+"use client";
+export default function Fileacase() {
+    return (
+      <>
+        <div>
+            Fileacase 
+      </div>
+      </>
+    );
+  }
+  
---
+++ src/app/page/trackcase/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Trackcase() {
+    return (
+        <>
+        Track Case
+        </>
+    )
+}
